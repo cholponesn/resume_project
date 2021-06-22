76356122c7ceff989f314bcf0dd82f2dd1525062
cholponesn <cholponkaimova653@example.com>
2021-06-22 18:25:26 +0600

    models files add new class

---
+++ resume_project/resume/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+class Resume(models.Model):
+    pass
+
+
+
+
+class Personalinfo(models.Model):
+    full_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    date_birth = models.DateField()
+    phone = models.IntegerField()
+    address = models.CharField(max_length=10)
+    photo = models.ImageField(null=True, blank=True)
+    bio = models.CharField(max_length=500)
+    resume = models.ForeignKey(Resume,on_delete=models.SET_NULL,null=True)
+
+class Education(models.Model):
+    start_year = models.IntegerField()
+    end_year = models.IntegerField(null=True)
+    school_name = models.CharField(max_length=15)
+    specialization = models.CharField(max_length=100)
+    resume = models.ForeignKey(Resume,on_delete=models.SET_NULL,null=True)
+
+
+class Experience(models.Model):
+    start_year = models.IntegerField()
+    end_year = models.IntegerField(null=True)
+    company = models.CharField(max_length=15)
+    responsibilities = models.CharField(max_length=500)
+    resume = models.ForeignKey(Resume,on_delete=models.SET_NULL,null=True)
+
+
+
+
+
+
+
+
