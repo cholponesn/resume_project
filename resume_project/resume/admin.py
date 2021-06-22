5ff629f6208dae5626c6b62a52bb967e65d94f84
cholponesn <cholponkaimova653@example.com>
2021-06-22 18:24:08 +0600

    admin files, add new class

---
+++ resume_project/resume/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register([Resume,Personalinfo,Education,Experience])
+
---
+++ resume_project/user_profile/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'photo']
+
+admin.site.register(Profile,ProfileAdmin)
+
+
+
