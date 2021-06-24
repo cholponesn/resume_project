f0ae977481502c3678667cb70e8371d2dd96a52a
cholponesn <cholponkaimova653@example.com>
2021-06-24 18:03:05 +0600

    add urls id

--- resume_project/resume/urls.py
+++ resume_project/resume/urls.py
@@ -3,9 +3,9 @@ from .views import *
 
 urlpatterns = [
    path('resume/',ResumeView.as_view()),
-   path('personalinfo/',PersonalinfoView.as_view()),
-   path('education/',EducationView.as_view()),
-   path('experience/',ExperienceView.as_view()),
+   path('personalinfo/<int:resume_id>/',PersonalinfoView.as_view(),name='personalinfo'),
+   path('education/<int:resume_id>/',EducationView.as_view(),name='education'),
+   path('experience/<int:resume_id>/',ExperienceView.as_view(),name='experience'),
 
 
 ]
