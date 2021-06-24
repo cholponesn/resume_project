3526a3d0994980bc2b2194bc92eadf41e504d006
cholponesn <cholponkaimova653@example.com>
2021-06-24 15:33:52 +0600

    register and resume view

--- resume_project/resume/views.py
+++ resume_project/resume/views.py
@@ -1,3 +1,66 @@
+from django.http import request
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import *
+from rest_framework.response import Response
+from .models import *
 
-# Create your views here.
+class ResumeView(APIView):
+
+    def get(self,request):
+        resume = Resume.objects.all()
+        serializer = ResumeSerializer(resume,many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = ResumeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+
+
+
+
+class PersonalinfoView(APIView):
+
+    def post(self,request):
+        serializer = PersonalinfoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors,status=400)
+
+class EducationView(APIView):
+    def post(self,request):
+        serializer = EducationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors,status=400)
+
+class ExperienceView(APIView):
+    def post(self,request):
+        serializer = ExperienceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors,status=400)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- resume_project/user_profile/views.py
+++ resume_project/user_profile/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import *
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self,request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response('Successfuly register!', status=201)
+        return Response(serializer.errors, status=400)
+
+
