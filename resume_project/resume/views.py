6a8a5b65cbd3f68b4e1b1560c2d9133f8d594803
cholponesn <cholponkaimova653@example.com>
2021-06-24 18:02:43 +0600

    test view

--- resume_project/resume/views.py
+++ resume_project/resume/views.py
@@ -8,44 +8,63 @@ from .models import *
 class ResumeView(APIView):
 
     def get(self,request):
-        resume = Resume.objects.all()
+        resume = Resume.objects.filter(user=request.user)
         serializer = ResumeSerializer(resume,many=True)
         return Response(serializer.data)
 
     def post(self,request):
-        serializer = ResumeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-
-
+        new_resume = Resume.objects.create(user=request.user)
+        serializer = ResumeSerializer(new_resume)
+        return Response(serializer.data, status=201)
 
 
 
 class PersonalinfoView(APIView):
 
-    def post(self,request):
+    def post(self,request,**kwargs):
+        resume = Resume.objects.get(id=kwargs['resume_id'])
         serializer = PersonalinfoSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            Personalinfo.objects.create(full_name=serializer.data.get('full_name'),
+                                        email=serializer.data.get('email'),
+                                        date_birth=serializer.data.get('date_birth'),
+                                        phone=serializer.data.get('phone'),
+                                        address=serializer.data.get('address'),
+                                        photo=serializer.data.get('photo'),
+                                        bio=serializer.data.get('bio'),
+                                        resume=resume)
+
+
             return Response(serializer.data,status=201)
         return Response(serializer.errors,status=400)
 
 class EducationView(APIView):
-    def post(self,request):
+    def post(self,request,**kwargs):
+        resume = Resume.objects.get(id=kwargs['resume_id'])
+
         serializer = EducationSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            Education.objects.create(start_year=serializer.data.get('start_year'),
+                                     end_year=serializer.data.get('end_year'),
+                                     school_name=serializer.data.get('school_name'),
+                                     specialization=serializer.data.get('specialization'),
+                                     resume=resume)
+
             return Response(serializer.data,status=201)
         return Response(serializer.errors,status=400)
 
 class ExperienceView(APIView):
-    def post(self,request):
+    def post(self,request,**kwargs):
+        resume = Resume.objects.get(id=kwargs['resume_id'])
+
         serializer = ExperienceSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            Experience.objects.create(start_year=serializer.data.get('start_year'),
+                                      end_year=serializer.data.get('end_year'),
+                                      company=serializer.data.get('company'),
+                                      responsibilities=serializer.data.get('responsibilities'),
+                                      resume=resume)
+
             return Response(serializer.data,status=201)
         return Response(serializer.errors,status=400)
 
